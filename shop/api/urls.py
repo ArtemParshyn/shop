90ee37580428d3e9f988041f8489ff4fc5463287
Artem Parshyn <parshyn4@gmail.com>
2024-09-29 01:13:58 +0300

    all templates were fixed and are ready to work with.

--- shop/api/urls.py
+++ shop/api/urls.py
@@ -3,5 +3,13 @@ from django.urls import path
 from api import views
 
 urlpatterns = [
-        path('', views.index, name='index')
+        path('nav', views.nav, name="nav"),
+        path('head', views.head, name="head"),
+        path('orders', views.orders, name="order"),
+        path('cardstore', views.cardstore, name="cardstore"),
+        path('topup', views.topup, name="topup"),
+        path('', views.index, name='index'),
+        path('dyn_top', views.dynamic_topics, name='dynamic_topics'),
+        #path('login', views.login, name="login"),
+        #path('reg', views.reg, name="reg"),
 ]
--- shop/api/views.py
+++ shop/api/views.py
@@ -1,7 +1,37 @@
+from django.contrib import auth
+from django.http import HttpResponseRedirect
 from django.shortcuts import render
+from django.urls import reverse
+
+from api import forms
 
 
 # Create your views here.
-def index(request):
+def nav(request):
+    return render(request, 'nav.html')
 
+
+def orders(request):
+    return render(request, 'Orders - ElonMoney Shop.html')
+
+
+def topup(request):
+    return render(request, 'Topup - ElonMoney Shop.html')
+
+
+def cardstore(request):
+    return render(request, 'Purchase Cards - ElonMoney Shop.html')
+
+
+def dynamic_topics(request):
+    return render(request, 'Dynamic Topups - ElonMoney Shop.html')
+
+
+def head(request):
+    return render(request, 'header.html')
+
+
+def index(request):
     return render(request, 'Dashboard - ElonMoney Shop.html')
+
+
